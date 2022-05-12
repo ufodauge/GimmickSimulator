c16226d5c053b7b8035c2b56bbcfb2e2ae7aa8e4
agdf_ydy <hakka.ydy.sub@gmail.com>
2022-05-12 18:11:29 +0900

    master

--- data/font.lua
+++
@@ -1,5 +0,0 @@
-FONT_REGULAR_SIZE = 24
-FONT_REGULAR = love.graphics.newFont( 'resource/PixelMplus12-Regular.ttf', FONT_REGULAR_SIZE ):setFilter( 'nearest', 'nearest' )
-
-FONT_TITLE_SIZE = 64
-FONT_TITLE = love.graphics.newFont( 'resource/PixelMplus12-Regular.ttf', FONT_TITLE_SIZE ):setFilter( 'nearest', 'nearest' )
